e8aa9b7261cd0932f7d516c1c9bc093be29376a1
Shail Pradhan <shail.pradhan@icloud.com>
2025-04-28 18:04:30 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://quiz-app-server-kjh8.onrender.com/api" });
 
 // Fetch all quizzes
 export const fetchQuizzes = () => API.get("/quizzes");
